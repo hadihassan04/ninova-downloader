f5a96bacf3a5f1b4e14cd737245d10cc05038b9e
erfur <erfur@itu.edu.tr>
2017-09-18 11:36:23 +0300

    added html.parser

--- ninova_downloader.py
+++ ninova_downloader.py
@@ -8,7 +8,7 @@ cookies = dict(ITUGirisV3=loginKey)
 kampusPage = requests.get('http://ninova.itu.edu.tr/Kampus1',
                           cookies=cookies)
 
-kampusSoup = bs4.BeautifulSoup(kampusPage.text)
+kampusSoup = bs4.BeautifulSoup(kampusPage.text, 'html.parser')
 
 for line in kampusSoup.find_all('a'):
     if 'ErisimAgaci' in str(line):
