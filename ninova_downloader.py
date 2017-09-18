3051ea1c931f250c246620aeea3d347b4f026ac8
erfur <erfur@itu.edu.tr>
2017-09-18 11:34:19 +0300

    Get the main page and parse the class links

---
+++ ninova_downloader.py
@@ -0,0 +1,15 @@
+import requests
+import bs4
+
+credFile = open('cred')
+loginKey = credFile.readline()
+cookies = dict(ITUGirisV3=loginKey)
+
+kampusPage = requests.get('http://ninova.itu.edu.tr/Kampus1',
+                          cookies=cookies)
+
+kampusSoup = bs4.BeautifulSoup(kampusPage.text)
+
+for line in kampusSoup.find_all('a'):
+    if 'ErisimAgaci' in str(line):
+        print(line)
