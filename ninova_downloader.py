72e1846c87f2bb6c6e124e1cf3458f11860d7a56
erfur <erfur@itu.edu.tr>
2017-09-18 20:56:17 +0300

    pull folders and list file links

--- ninova_downloader.py
+++ ninova_downloader.py
@@ -3,6 +3,7 @@ import bs4
 
 credFile = open('cred')
 loginKey = credFile.readline()
+credFile.close()
 cookies = dict(ITUGirisV3=loginKey)
 
 kampusPage = requests.get('http://ninova.itu.edu.tr/Kampus1',
@@ -10,6 +11,15 @@ kampusPage = requests.get('http://ninova.itu.edu.tr/Kampus1',
 
 kampusSoup = bs4.BeautifulSoup(kampusPage.text, 'html.parser')
 
+classes = []
 for line in kampusSoup.find_all('a'):
     if 'ErisimAgaci' in str(line):
-        print(line)
+        classes.append(line['href'])
+
+for classLink in classes:
+    classPage = requests.get('http://ninova.itu.edu.tr' + classLink +
+                             '/DersDosyalari', cookies=cookies)
+    classPageSoup = bs4.BeautifulSoup(classPage.text, 'html.parser')
+    for link in classPageSoup.find_all('a'):
+        if 'DersDosyalari?' in str(link):
+            print(link['href'])
