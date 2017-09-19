4df04766a7b5c791004684a77ab3547ce3dccbeb
erfur <erfur@itu.edu.tr>
2017-09-19 15:41:34 +0300

    refactored the code

--- ninova_downloader.py
+++ ninova_downloader.py
@@ -1,25 +1,34 @@
 import requests
 import bs4
 
-credFile = open('cred')
-loginKey = credFile.readline()
-credFile.close()
-cookies = dict(ITUGirisV3=loginKey)
 
-kampusPage = requests.get('http://ninova.itu.edu.tr/Kampus1',
-                          cookies=cookies)
+def loadCreds():
+    credFile = open('cred')
+    loginKey = credFile.readline()
+    credFile.close()
+    return loginKey
 
-kampusSoup = bs4.BeautifulSoup(kampusPage.text, 'html.parser')
 
-classes = []
-for line in kampusSoup.find_all('a'):
-    if 'ErisimAgaci' in str(line):
-        classes.append(line['href'])
+def getPage(url):
+    cookies = dict(ITUGirisV3=loadCreds())
+    kampusPage = requests.get(url, cookies=cookies)
+    return bs4.BeautifulSoup(kampusPage.text, 'html.parser')
 
-for classLink in classes:
-    classPage = requests.get('http://ninova.itu.edu.tr' + classLink +
-                             '/DersDosyalari', cookies=cookies)
-    classPageSoup = bs4.BeautifulSoup(classPage.text, 'html.parser')
-    for link in classPageSoup.find_all('a'):
-        if 'DersDosyalari?' in str(link):
-            print(link['href'])
+
+def getLinks(soup, filter):
+    classes = []
+    for line in soup.find_all('a'):
+        if filter in str(line):
+            classes.append(line['href'])
+    return classes
+
+
+url = 'http://ninova.itu.edu.tr/Kampus1'
+kampusSoup = getPage('http://ninova.itu.edu.tr/Kampus1')
+classLinks = getLinks(kampusSoup, 'ErisimAgaci')
+
+for link in classLinks:
+    page = getPage(url+link+'/DersDosyalari', cookies=loadCreds())
+    links = getLinks(page, 'DersDosyalari?')
+    for i in links:
+        print(link['href'])
